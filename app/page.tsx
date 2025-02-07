c20496087b1a852cc279f2480bef335609c021dc
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-02-07 18:06:16 +0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function pages() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
       <div className="text-4xl text-gray-900 font-bold mb-4">
-        Welcome to Noodle's Den's Landing Page
+        Welcome to Noodle&apos;s Den&apos;s Landing Page
       </div>
       <div className="text-xl text-gray-600">
         This website is still a work in progress
