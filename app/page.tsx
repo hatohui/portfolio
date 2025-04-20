cf3e4864fc4bc2e0c8e76b01f3954814764b3b68
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-04-20 16:53:28 +0700

    nit: some ux

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,148 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import CharacterCard, { HoverCards } from "@/components/Landing/CharacterCard";
+import TypedText from "@/components/Utils/TypedText";
 import { useState } from "react";
-
-type HoverCards = "BRYAN" | "HATOHUI" | undefined;
 
 export default function Pages() {
   const [hovered, setHovered] = useState<HoverCards>();
-  const router = useRouter();
 
   const imagesSrc = {
-    hatohui: "/Images/me_drag.png",
+    hatohui: "/Images/meme!.png",
     zagvandr: "/Images/BryanBanner.jpg",
   };
 
-  const userPath = {
-    hatohui: "/hatohui",
-    zagvandr: "/zagvandr",
-  };
-
   return (
-    <>
-      <div className="fixed top-0 left-0 w-screen h-screen bg-[url('/Images/axiom-pattern.png')]" />
+    <div className="overflow-hidden">
+      {hovered == "ZAG" && (
+        <>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-2 bg-gradient-to-r from-green-500 via-cyan-400 to-blue-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_3s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-2 bg-gradient-to-r from-yellow-300 via-yellow-600 to-orange-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_10s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-7 bg-gradient-to-r from-pink-300 via-purple-500 to-red-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_6s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-7 bg-gradient-to-r from-pink-300 via-purple-500 to-red-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_2s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-12 bg-gradient-to-r from-blue-400 via-teal-500 to-green-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_5s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-2 bg-gradient-to-r from-blue-500 via-cyan-400 to-green-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_3s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-2 bg-gradient-to-r from-orange-500 via-yellow-600 to-yellow-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_10s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-7 bg-gradient-to-r from-red-400 via-purple-500 to-pink-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_6s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-7 bg-gradient-to-r from-red-400 via-purple-500 to-pink-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_2s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-12 bg-gradient-to-r from-green-400 via-teal-500 to-blue-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_5s_ease-in-out_infinite]" />
+        </>
+      )}
+      {hovered == "HAT" && (
+        <>
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-2 bg-gradient-to-r from-blue-500 via-cyan-400 to-green-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_3s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-2 bg-gradient-to-r from-orange-500 via-yellow-600 to-yellow-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_10s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-7 bg-gradient-to-r from-red-400 via-purple-500 to-pink-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_6s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-7 bg-gradient-to-r from-red-400 via-purple-500 to-pink-300 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_2s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute select-none z-[9999] mt-12 bg-gradient-to-r from-green-400 via-teal-500 to-blue-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-right_5s_ease-in-out_infinite]" />
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-2 bg-gradient-to-r from-green-500 via-cyan-400 to-blue-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_3s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-2 bg-gradient-to-r from-yellow-300 via-yellow-600 to-orange-500 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_10s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-7 bg-gradient-to-r from-pink-300 via-purple-500 to-red-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_6s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-7 bg-gradient-to-r from-pink-300 via-purple-500 to-red-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_2s_ease-in-out_infinite]"></div>
+          <div className="w-screen h-2 absolute bottom-0 select-none z-[9999] mb-12 bg-gradient-to-r from-blue-400 via-teal-500 to-green-400 [mask-image:url('/Images/shattered-dark.png')] rounded-xl animate-[scroll-left_5s_ease-in-out_infinite]"></div>
+        </>
+      )}
+
+      {hovered == "ZAG" && (
+        <div
+          className={`absolute lg:bottom-[10%] bottom-36 lg:top-auto pointer-events-none w-full md:w-auto text-center md:top-16 md:right-0 lg:right-[15%] z-[9999] duration-300 overflow-hidden transition-opacity ${
+            hovered ? "opacity-100" : "opacity-0"
+          } select-none drop-shadow-lg`}
+        >
+          <TypedText
+            className="font-mono lg:text-9xl text-7xl text-[#b48d04] animate-pulse overflow-hidden font-extrabold"
+            strings={["ZAGVANDR"]}
+            typeSpeed={60}
+          />
+        </div>
+      )}
+
+      {hovered == "HAT" && (
+        <div
+          className={`absolute lg:top-[20%] w-full md:w-auto md:left-0 pointer-events-none top-44 md:top-16 lg:left-auto lg:right-[15%] z-[9999] text-center duration-300 overflow-hidden transition-opacity ${
+            hovered ? "opacity-100" : "opacity-0"
+          } select-none drop-shadow-lg`}
+        >
+          <TypedText
+            className="font-mono lg:text-9xl text-[#b48d04] text-7xl animate-pulse font-extrabold"
+            strings={["HATOHUI"]}
+            typeSpeed={60}
+          />
+        </div>
+      )}
+
       <div
-        className={`fixed w-screen h-screen z-10 opacity-80 ${
+        className={`${
+          hovered ? "z-20" : "opacity-0 -z-40"
+        } fixed w-screen h-screen transition-opacity [mask-image:url('/Images/axiom-pattern.png')] pointer-events-none animate-pulse brightness-200 duration-200 ${
           hovered
-            ? hovered === "BRYAN"
+            ? hovered === "ZAG"
               ? "bg-[#bb5a1a]"
               : "bg-green-600"
-            : "hidden"
+            : "opacity-0"
         }`}
       ></div>
-      <div
-        className={`fixed w-screen h-screen z-10 opacity-80 ${
-          hovered ? "bg-[url('/Images/white-tiles.png')]" : "hidden"
-        }`}
-      ></div>
-      <div className="flex overflow-hidden flex-col z-50 justify-center text-center items-center h-screen select-none w-full">
-        <div
-          className="w-full h-1/2 cursor-pointer hover:z-50 hover:h-2/3 transition-transform flex flex-nowrap border-2 flex-col focus:scale-95 hover:border-t-2 border-b-2 duration-300 ease-out hover:translate-y-10 hover:scale-105 relative bg-slate-950 hover:bg-[#aaeffe] skew-y-6 overflow-hidden group"
-          onMouseEnter={() => setHovered("BRYAN")}
-          onMouseLeave={() => setHovered(undefined)}
-          onClick={() => router.push(userPath.zagvandr)}
-        >
-          <div className="z-30 group group-hover:opacity-70 opacity-0 group text-[16em] group-hover:animate-[scroll-left_6000ms_linear_infinite] font-bold flex items-center overflow-hidden select-none w-full">
-            <p className="shadow-lg">ZAGVANDR</p>
-          </div>
-          <div className="absolute bg-gradient-to-b from-slate-950 via-transparent to-slate-950 h-full w-full z-20 opacity-0 group-hover:opacity-30"></div>
-          <img
-            className="w-full h-full absolute object-cover group-hover:object-[center_40%] object-[center_25%] transition-[object-position,transform] group-hover:translate-x-20 opacity-40 group-hover:opacity-100 ease-linear group-hover:duration-[20000ms]"
-            src={imagesSrc.zagvandr}
-            alt="Smexy fox picture uwu"
-          />
-          <div className="z-30 group-hover:opacity-70 opacity-0 group text-[16em] group-hover:animate-[scroll-l_6000ms_linear_infinite_reverse] font-bold flex items-center overflow-hidden select-none w-full">
-            <p className="shadow-lg">ZAGVANDR</p>
-          </div>
-        </div>
 
-        <div
-          className="w-full h-1/2 hover:h-2/3 cursor-pointer transition-transform flex flex-nowrap flex-col duration-300 border-t-2 border-2 hover:border-b-2 ease-out hover:z-50 hover:-translate-y-10 hover:scale-105 relative bg-slate-950 hover:bg-[#aaeffe] skew-y-6 overflow-hidden group"
-          onMouseEnter={() => setHovered("HATOHUI")}
-          onMouseLeave={() => setHovered(undefined)}
-          onClick={() => router.push(userPath.hatohui)}
-        >
-          <div className="z-30 group-hover:opacity-70 opacity-0 group text-[16em] group-hover:animate-[scroll-left_6000ms_linear_infinite_reverse] font-bold flex items-center overflow-hidden select-none w-full">
-            <p className="shadow-lg">HATOHUI</p>
-          </div>
-          <div className="absolute bg-gradient-to-b from-slate-950 via-transparent to-slate-950 h-full w-full z-20 opacity-0 group-hover:opacity-30"></div>
-          <img
-            className="w-full h-full absolute object-cover group-hover:object-[center_20%] object-[center_60%] transition-[object-position,transform] group-hover:translate-x-20 opacity-40 group-hover:opacity-100 ease-linear group-hover:duration-[20000ms]"
-            src={imagesSrc.hatohui}
-            alt="Smexy fox picture uwu"
-          />
-          <div className="z-30 group-hover:opacity-70 opacity-0 group text-[16em] group-hover:animate-[scroll-l_6000ms_linear_infinite] font-bold flex items-center overflow-hidden select-none w-full">
-            <p className="shadow-lg">HATOHUI</p>
-          </div>
-        </div>
+      {/* Images */}
+      <div className="flex h-screen w-screen absolute pointer-events-none overflow-hidden ">
+        <img
+          src="/Images/zagvandr.png"
+          className={`transition-all duration-300 brightness-0 scale-75 hidden md:block ${
+            hovered === "ZAG"
+              ? "brightness-100 translate-x-20 translate-y-0 scale-[1.03] z-[100]"
+              : "md:translate-y-20 md:-translate-10 z-[99]"
+          } ${hovered === "HAT" ? "opacity-5" : ""}`}
+        ></img>
       </div>
-    </>
+
+      <div className="flex h-screen w-screen absolute pointer-events-none overflow-hidden">
+        <img
+          src="/Images/Hatohui.webp"
+          className={`transition-all duration-300 brightness-0 object-cover scale-75 hidden md:block ${
+            hovered === "HAT"
+              ? "brightness-100 translate-x-20 translate-y-0 scale-[1.05] z-[100]"
+              : "md:translate-y-20 md:translate-x-20 z-[99]"
+          } ${hovered === "ZAG" ? "opacity-5" : ""}`}
+        ></img>
+      </div>
+
+      <div
+        className={`${
+          hovered ? "opacity-8" : "opacity-0"
+        } fixed w-screen h-screen z-20 bg-gradient-to-t from-black via-transparent to-black pointer-events-none duration-200 transition-opacity`}
+      ></div>
+
+      <div
+        className={`${
+          hovered ? "" : "hidden"
+        } fixed w-screen h-screen z-10 bg-black opacity-90 pointer-events-none`}
+      ></div>
+
+      {/* Cards */}
+      <div className="flex flex-col h-dvh justify-center overflow-hidden gap-5 select-none">
+        <CharacterCard
+          setHovered={setHovered}
+          className="hover:translate-y-14 hover:duration-700 focus:translate-y-14 focus:duration-700 duration-200"
+          imageStyle="group-hover:object-[center_35%] group-focus:object-[center_35%] object-[center_30%]"
+          hoverValue="ZAG"
+          to="/zagvandr"
+          imgSrc={imagesSrc.zagvandr}
+          alt="fox husband"
+          label="ZAGVANDR"
+        />
+        <CharacterCard
+          setHovered={setHovered}
+          className="hover:-translate-y-14 hover:duration-700 focus:-translate-y-14 focus:duration-700 duration-200"
+          imageStyle="group-hover:object-[center_20%] group-focus:object-[center_20%] object-[center_10%]"
+          hoverValue="HAT"
+          to="/hatohui"
+          imgSrc={imagesSrc.hatohui}
+          alt="noodle"
+          label="HATOHUI"
+        />
+      </div>
+
+      <div className="fixed top-0 left-0 w-screen h-screen bg-[url('/Images/axiom-pattern.png')] -z-50" />
+    </div>
   );
 }
---
+++ components/Landing/CharacterCard.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { SetStateAction } from "react";
+
+export type HoverCards = "ZAG" | "HAT" | undefined;
+
+export type CharacterCardProps = {
+  setHovered: React.Dispatch<SetStateAction<HoverCards>>;
+  to: string;
+  imgSrc: string;
+  alt: string;
+  label: string;
+  hoverValue: HoverCards;
+  className?: string;
+  imageStyle?: string;
+};
+
+const CharacterCard = ({
+  setHovered,
+  to,
+  imgSrc,
+  alt,
+  label,
+  hoverValue,
+  imageStyle,
+  className,
+}: CharacterCardProps) => {
+  const router = useRouter();
+
+  return (
+    <button
+      className={`${
+        className ?? ""
+      } h-1/3 cursor-pointer transition-transform flex flex-col focus:z-50 hover:z-50 focus:scale-110 hover:scale-110 relative skew-y-6 overflow-visible origin-center group cursor-[url('/Images/paw.png'),pointer]`}
+      onMouseEnter={() => setHovered(hoverValue)}
+      onMouseLeave={() => setHovered(undefined)}
+      onClick={() => router.push(to)}
+      onFocus={() => setHovered(hoverValue)}
+      onBlur={() => setHovered(undefined)}
+      onTouchStart={() => setHovered(hoverValue)}
+      onTouchEnd={() => setHovered(undefined)}
+    >
+      <div className="absolute bg-[url('/Images/Select.png')] bg-no-repeat h-full z-50"></div>
+      <div className="absolute flex justify-end inset-0 z-[60] opacity-35">
+        <div className="w-full h-full bg-gradient-to-t from-black via-transparent to-black skew-y-[-6] origin-top" />
+      </div>
+      <div className="absolute -top-[1.2rem] left-0 w-full h-5 max-h-5 bg-[url('/Images/caution-tape.jpg')] animate-pulse bg-repeat-x z-20 opacity-90 brightness-[.2] group-focus:brightness-[2] group-hover:brightness-[2] transition-[filter] duration-200 ease-in-out" />
+      <div className="absolute -bottom-[1.2rem] left-0 w-full h-5 max-h-5 bg-[url('/Images/caution-tape.jpg')] animate-pulse bg-repeat-x z-20 opacity-90 brightness-[.2] group-focus:brightness-[.9] group-hover:brightness-[.9] transition-[filter] duration-200 ease-in-out" />
+      <div className="absolute group-hover:opacity-0 group-focus:opacity-0 opacity-60 transition-opacity bg-black w-full h-full duration-300 ease-out"></div>
+      <img
+        className={`${
+          imageStyle ??
+          "group-hover:object-[center_35%] group-focus:object-[center_35%] object-[center_30%]"
+        } w-full h-full object-cover transition-[object-position,transform] lg:group-hover:translate-x-20 lg:group-focus:translate-x-20 ease-linear group-hover:duration-[7000ms] group-focus:duration-[7000ms]`}
+        src={imgSrc}
+        alt={alt}
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-transparent via-orange-400 to-transparent h-full w-full scale-150 opacity-0 group-hover:opacity-35 group-hover:blur-sm group-focus:opacity-35 group-focus:blur-sm transition-all duration-300 pointer-events-none -z-40" />
+    </button>
+  );
+};
+
+export default CharacterCard;
